9a623ef0570eaa2329f8704cbb211c5623f25576
dewei.ma <xnmageis@outlook.com>
2026-07-22 23:03:31 +0800

    feat: add ignore dir

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -1,48 +1,48 @@
 return {
-    "nvim-telescope/telescope.nvim",
-    cmd = "Telescope",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-        "xiyaowong/telescope-emoji.nvim",
-    },
-    keys = {
-        { "<C-p>", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
-        { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
-        { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live Grep" },
-        { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help Tags" },
-        { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
-        { "<leader>fe", "<cmd>Telescope emoji<cr>", desc = "Emoji" },
-        { "<leader>fo", "<cmd>Telescope oldfiles<cr>", desc = "Old Files" },
-    },
-    config = function()
-        local telescope = require("telescope")
-        local actions = require("telescope.actions")
+  "nvim-telescope/telescope.nvim",
+  cmd = "Telescope",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    "xiyaowong/telescope-emoji.nvim",
+  },
+  keys = {
+    { "<C-p>", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+    { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+    { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live Grep" },
+    { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help Tags" },
+    { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
+    { "<leader>fe", "<cmd>Telescope emoji<cr>", desc = "Emoji" },
+    { "<leader>fo", "<cmd>Telescope oldfiles<cr>", desc = "Old Files" },
+  },
+  config = function()
+    local telescope = require("telescope")
+    local actions = require("telescope.actions")
 
-        telescope.setup({
-            defaults = {
-                prompt_prefix = "🔍 ",
-                selection_caret = "➤ ",
+    telescope.setup({
+      defaults = {
+        prompt_prefix = "🔍 ",
+        selection_caret = "➤ ",
 
-                file_ignore_patterns = { "node_modules", "%.lock" },
+        file_ignore_patterns = { "node_modules", "%.lock", ".git", "dist", ".venv" },
 
-                mappings = {
-                    i = {
-                        ["<C-j>"] = actions.move_selection_next,
-                        ["<C-k>"] = actions.move_selection_previous,
-                        ["<esc>"] = actions.close,
-                    },
-                },
-            },
-            pickers = {
-                find_files = {
-                    hidden = true, -- 显示隐藏文件
-                },
-            },
-        })
+        mappings = {
+          i = {
+            ["<C-j>"] = actions.move_selection_next,
+            ["<C-k>"] = actions.move_selection_previous,
+            ["<esc>"] = actions.close,
+          },
+        },
+      },
+      pickers = {
+        find_files = {
+          hidden = true, -- 显示隐藏文件
+        },
+      },
+    })
 
-        -- 加载扩展
-        pcall(telescope.load_extension, "emoji")
-        pcall(telescope.load_extension, "fzf")
-    end,
+    -- 加载扩展
+    pcall(telescope.load_extension, "emoji")
+    pcall(telescope.load_extension, "fzf")
+  end,
 }
