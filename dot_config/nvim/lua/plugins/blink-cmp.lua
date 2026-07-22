9478f996e9b6894aee7638214955b4e323ac8cdd
dewei.ma <xnmageis@outlook.com>
2026-07-22 22:04:51 +0800

    feat: 确保依赖加载顺序

--- dot_config/nvim/lua/plugins/blink-cmp.lua
+++ dot_config/nvim/lua/plugins/blink-cmp.lua
@@ -1,6 +1,7 @@
 return {
   "saghen/blink.cmp",
   version = "v1.*",
+  dependencies = { "L3MON4D3/LuaSnip" },
   opts = {
     keymap = { preset = "super-tab" },
     appearance = { nerd_font_variant = "mono" },
