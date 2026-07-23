cbcb44590407240539307667c4155c3edccaab00
dewei.ma <xnmageis@outlook.com>
2026-07-23 21:51:58 +0800

    chore(nvim): remove neogit plugin
    Neogit overlaps with gitsigns + diffview functionality and is rarely used.
    Removed in favor of simpler git workflow.

--- dot_config/nvim/lua/plugins/neogit.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "NeogitOrg/neogit",
-  cmd = "Neogit",
-  dependencies = { "nvim-lua/plenary.nvim", "sindrets/diffview.nvim" },
-  keys = {
-    { "<leader>gg", "<cmd>Neogit<cr>", desc = "Open Neogit" },
-  },
-  opts = {},
-}
