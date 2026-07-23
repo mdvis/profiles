6515fb8dd8408997ad7fd864076873fce4b2aee1
dewei.ma <xnmageis@outlook.com>
2026-07-23 21:03:25 +0800

    feat: 修正 setup 调用

--- dot_config/nvim/lua/plugins/nvim-treesitter.lua
+++ dot_config/nvim/lua/plugins/nvim-treesitter.lua
@@ -31,6 +31,6 @@ return {
     indent = { enable = true },
   },
   config = function(_, opts)
-    require("nvim-treesitter").setup(opts)
+    require("nvim-treesitter.configs").setup(opts)
   end,
 }
