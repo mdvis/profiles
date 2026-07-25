a3131ee381b17846e4a89654c6d75488bbf057d4
dewei.ma <xnmageis@outlook.com>
2026-07-25 13:27:53 +0800

    fix(nvim): 修复 gitsigns 废弃的 next_hunk/prev_hunk API
    将 gs.next_hunk() / prev_hunk() 改为 gs.nav_hunk("next") / nav_hunk("prev")，
    对齐 gitsigns 最新 API。

--- dot_config/nvim/lua/plugins/gitsigns.lua
+++ dot_config/nvim/lua/plugins/gitsigns.lua
@@ -27,13 +27,13 @@ return {
       -- Navigation
       map('n', ']c', function()
         if vim.wo.diff then return ']c' end
-        vim.schedule(function() gs.next_hunk() end)
+        vim.schedule(function() gs.nav_hunk("next") end)
         return '<Ignore>'
       end, { expr = true, desc = "Next git hunk" })
-      
+
       map('n', '[c', function()
         if vim.wo.diff then return '[c' end
-        vim.schedule(function() gs.prev_hunk() end)
+        vim.schedule(function() gs.nav_hunk("prev") end)
         return '<Ignore>'
       end, { expr = true, desc = "Previous git hunk" })
     end
