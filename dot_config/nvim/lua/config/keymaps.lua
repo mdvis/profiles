1429082c2afc5bcec0eb29c9ff089f20de198db4
dewei.ma <xnmageis@outlook.com>
2026-07-26 21:03:33 +0800

    fix(nvim): E488 trailing characters 错误，改用 Lua 函数处理 gj/gk 映射

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -3,6 +3,10 @@ local map = vim.keymap.set
 map("n", "<leader><leader>t", "<cmd>SetTpl<cr>", { silent = true, desc = "Set template" })
 map("n", "<leader><cr>", "<cmd>noh<cr>", { silent = true, desc = "Clear search highlight" })
 
-map("n", "j", "v:count == 0 and 'gj' or 'j'", { expr = true, silent = true, desc = "Move down by visual line" })
-map("n", "k", "v:count == 0 and 'gk' or 'k'", { expr = true, silent = true, desc = "Move up by visual line" })
+map("n", "j", function()
+  return vim.v.count == 0 and "gj" or "j"
+end, { expr = true, silent = true, desc = "Move down by visual line" })
+map("n", "k", function()
+  return vim.v.count == 0 and "gk" or "k"
+end, { expr = true, silent = true, desc = "Move up by visual line" })
 map("n", "0", "^", { desc = "Go to first non-blank character" })
