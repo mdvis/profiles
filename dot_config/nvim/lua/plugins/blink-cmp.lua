7bdae3af0ee98f268eb130a7a153cee3addbc7a0
dewei.ma <xnmageis@outlook.com>
2026-09-18 19:39:21 +0800

    perf(nvim): blink-cmp 改为 InsertEnter 懒加载
    避免空 buffer 启动时即加载 blink.cmp；打开文件时仍由
    nvim-lspconfig 依赖连带加载，行为不变

--- dot_config/nvim/lua/plugins/blink-cmp.lua
+++ dot_config/nvim/lua/plugins/blink-cmp.lua
@@ -1,6 +1,7 @@
 return {
   "saghen/blink.cmp",
   version = "v1.*",
+  event = "InsertEnter",
   dependencies = { "rafamadriz/friendly-snippets" },
   opts = {
     keymap = { preset = "super-tab" },
